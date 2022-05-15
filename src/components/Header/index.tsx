7e53aa10605d64db2d7de0649221bf1e2def7e42
pedro-severo <phsevero94@gmail.com>
2022-05-15 15:30:47 -0300

    removing user name from global state

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,12 +5,13 @@ import { PATH } from "../../routes/paths";
 import { HeaderView } from "./view";
 
 export const Header: React.FC<any> = () => {
-  const [userName] = useGlobal("userName");
+  const [userName, setUserName] = useGlobal("userName");
   const navigate = useNavigate();
 
   const handleGoBackClick = (): void => {
     localStorage.removeItem("userName");
     localStorage.removeItem("token");
+    setUserName("");
     navigate(`${PATH.LOGIN}`);
   };
 
